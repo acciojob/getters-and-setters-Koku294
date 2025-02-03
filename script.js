d54f5eb11602736c5ad499c1b9d314f305eccf7c
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-03 22:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-//complete this code
-class Person {}
+class Person {
+  // Constructor to initialize name and age
+  constructor(name, age) {
+    this._name = name;  // Private property for name
+    this._age = age;    // Private property for age
+  }
 
-class Student extends Person {}
+  // Getter for the name property
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for the age property
+  set age(age) {
+    this._age = age;
+  }
 
+  // Getter for the age property
+  get age() {
+    return this._age;
+  }
+}
+
+class Student extends Person {
+  // Method specific to Student class
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  // Method specific to Teacher class
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
